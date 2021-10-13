381f8a3933e10dad50a1a70ca467f36dfede7a85
Bhagya Mudgal <bhagyamudgal@gmail.com>
2021-10-13 15:31:16 +0530

    ignore eslint

--- next.config.js
+++ next.config.js
@@ -3,4 +3,9 @@ module.exports = {
   images: {
     domains: ["1000logos.net"],
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  }
 };
