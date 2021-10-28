4249cb7938e5732f137bb472938201a9ba950717
Bhagya Mudgal <bhagyamudgal@gmail.com>
2021-10-28 18:34:57 +0530

    resolved header display error on mobile devices

--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,7 @@ export default function Home() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
+    <div className="flex flex-col items-center justify-center">
       <Head>
         <title>Google Search Clone</title>
         <meta
